d44522d9d317df29b73605d2076e0b84796057b1
diaszakir <mailforstudy84@gmail.com>
2025-02-10 00:28:42 +0500

    try n1

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
